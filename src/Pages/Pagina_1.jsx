66cea41e7d96d4c6d8ab8b95188123996e46d255
Duarte <dionysiomariaduarte2808@gmail.com>
2025-03-27 08:42:08 -0300

    Pag 1

--- src/Pages/Pagina_1.jsx
+++ src/Pages/Pagina_1.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 function Pagina1() {
   // Estado que armazena as informações do país
-  const [info, setInfo] = useState(null);
+  const [infoPais, setInfoPais] = useState(null);
 
   // UseEffect para carregar as informações do país a partir do localStorage
   useEffect(() => {
@@ -11,7 +11,7 @@ function Pagina1() {
     const countryData = localStorage.getItem("countryInfo");
     if (countryData) {
       // Converte os dados do localStorage para um objeto e armazena no estado
-      setInfo(JSON.parse(countryData));
+      setInfoPais(JSON.parse(countryData));
     }
   }, []); // O array vazio significa que o efeito só ocorre quando o componente é montado
 
@@ -19,13 +19,13 @@ function Pagina1() {
     <div className="Flex-Colunm">
       <h2>Informações Gerais</h2>
       {/* Se as informações do país estiverem disponíveis, exibe-as */}
-      {info ? (
+      {infoPais ? (
         <>
-          <p>Área: {info.area} km²</p>
-          <p>População: {info.population}</p>
-          <p>Capital: {info.capital}</p>
-          <p>Região: {info.region}</p>
-          <p>Moeda: {Object.values(info.currencies)[0].name}</p>
+          <p>Área: {infoPais.area} km²</p>
+          <p>População: {infoPais.population}</p>
+          <p>Capital: {infoPais.capital}</p>
+          <p>Região: {infoPais.region}</p>
+          <p>Moeda: {Object.values(infoPais.currencies)[0].name}</p>
         </>
       ) : (
         // Caso não haja informações do país, exibe uma mensagem
